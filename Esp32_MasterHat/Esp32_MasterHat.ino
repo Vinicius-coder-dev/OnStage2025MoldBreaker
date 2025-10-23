67c4fe9dc5880cd7476531e50c2b352c6ced55cc
s0l0zero <guimaraesv650@gmail.com>
2025-10-23 16:13:45 -0300

    Update Esp32_MasterHat.ino
    Updated documentation

--- Esp32_MasterHat/Esp32_MasterHat.ino
+++ Esp32_MasterHat/Esp32_MasterHat.ino
@@ -15,27 +15,27 @@ BLEAdvertising *pAdvertising;
 // See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
 
-// Lista dos comandos, em que 0 siginifica que os comandos acaba ali
+// Command list, in which 0 means it ended
 char cards_commands[52][10] = {
-  { 0x05, 0x02, 0x01, 0x04, 0x02, 0x06, 0x03, 0x09, 0x06, 0x00}// Começo história 1,
-  { 0x06, 0x01, 0x02, 0x08, 0, 0, 0 ,0 ,0, 0 },// Escolha 1 história 1
-  { 0x06, 0x02, 0x02, 0x03, 0x04, 0x05, 0, 0, 0, 0 },// Escolha 2 história 1
-  { 0x06, 0x03, 0x02, 0x0A, 0x05, 0x01, 0x01, 0x0E, 0, 0 },// Continuação escolha 1 história 1
-  { 0x06, 0x04, 0x02, 0x0B, 0x03, 0x0C, 0x04, 0x0D, 0, 0 },// Continuaçaõ escolha 2 história 1
+  { 0x05, 0x02, 0x01, 0x04, 0x02, 0x06, 0x03, 0x09, 0x06, 0x00}// Start history 1
+  { 0x06, 0x01, 0x02, 0x08, 0, 0, 0 ,0 ,0, 0 },// Choice 1 history 1
+  { 0x06, 0x02, 0x02, 0x03, 0x04, 0x05, 0, 0, 0, 0 },// Choice 2 history 1
+  { 0x06, 0x03, 0x02, 0x0A, 0x05, 0x01, 0x01, 0x0E, 0, 0 },// Continuation choice 1 history 1
+  { 0x06, 0x04, 0x02, 0x0B, 0x03, 0x0C, 0x04, 0x0D, 0, 0 },// Continuation choice 2 history 1
   { 0x06, 0x05, 0, 0, 0, 0, 0, 0, 0, 0},
-  { 0x06, 0x06, 0, 0, 0, 0, 0, 0, 0, 0}, // Escolha 2 história 2
-  { 0x06, 0x07, 0, 0, 0, 0, 0, 0, 0, 0}, // Começo história 2
-  { 0x06, 0x08, 0, 0, 0, 0, 0, 0, 0, 0},
+  { 0x06, 0x06, 0, 0, 0, 0, 0, 0, 0, 0}, // Choice 2 history 2
+  { 0x06, 0x07, 0, 0, 0, 0, 0, 0, 0, 0}, // Start history 2
+  { 0x06, 0x08, 0, 0, 0, 0, 0, 0, 0, 0}, // Extra commands not utilized
   { 0x06, 0x09, 0, 0, 0, 0, 0, 0, 0, 0},
   { 0x06, 0x0A, 0, 0, 0, 0, 0, 0, 0, 0},
   { 0x06, 0x0B, 0, 0, 0, 0, 0, 0, 0, 0},
   { 0x06, 0x0C, 0, 0, 0, 0, 0, 0, 0, 0},
-  { 0x06, 0x0D, 0x05, 0x04, 0, 0, 0, 0, 0, 0},
+  { 0x06, 0x0D, 0x05, 0x04, 0, 0, 0, 0, 0, 0}, // Command to start the choice in the screen
   { 0x06, 0x0E, 0, 0, 0, 0, 0, 0, 0, 0},
-  { 0x06, 0x0F, 0, 0, 0, 0, 0, 0, 0, 0}, // Final e agradecimentos
-  { 0x06, 0x10, 0, 0, 0, 0, 0, 0, 0, 0}, // Escolha 1 história 2
-  { 0x06, 0x11, 0, 0, 0, 0, 0, 0, 0, 0}, // Continuação escolha 1 história 2
-  { 0x06, 0x12, 0, 0, 0, 0, 0 ,0 ,0 ,0} // Continuação escolha 2 história 2
+  { 0x06, 0x0F, 0, 0, 0, 0, 0, 0, 0, 0}, // End and thanking
+  { 0x06, 0x10, 0, 0, 0, 0, 0, 0, 0, 0}, // Choice 1 history 2
+  { 0x06, 0x11, 0, 0, 0, 0, 0, 0, 0, 0}, // Continuation choice 1 history 2 // not utilized
+  { 0x06, 0x12, 0, 0, 0, 0, 0 ,0 ,0 ,0} // Continuation choice 2 history 2 // not utilized
 };
 #define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
@@ -54,17 +54,16 @@ void setup() {
 
 void loop() {
   String card_detected = "";
-  //Verifica o UART da Raspberry
+  //Receives the command from UART
   if (Serial.available()) {
     card_detected = Serial.readStringUntil('\n');
   }
   //Serial.println(card_detected);
-  //Cards detection occurs here
 
   if (card_detected != "") {
 
     int number_of_commands = 0;
-    // Quantos comandos serão enviados
+    // Finding the number of commands necessary to broadcast
     for (int y = 0; y < 10; y++) {
       if (cards_commands[card_detected.toInt()][y] != 0) {
         number_of_commands++;
@@ -73,18 +72,18 @@ void loop() {
       }
     }
     Serial.println(number_of_commands);
-    //Lendo a lista de comandos de acordo com o número encontrado acima
+    //Finding commands to broadcast
     for (int i = 0; i < number_of_commands; i += 2) {
       BLEAdvertisementData advdata = BLEAdvertisementData();
       String sending_vector = "";
 
       char sending[7];
       //Setting the string to match the model of Spike
-      sending[0] = 0x97;
-      sending[1] = 0x03;
-      sending[2] = 0x01;
-      sending[3] = 0x61;
-      sending[5] = 0x61;
+      sending[0] = 0x97; //
+      sending[1] = 0x03; // 
+      sending[2] = 0x01; // The channel number, in this case 1
+      sending[3] = 0x61; //
+      sending[5] = 0x61; // 
 
 
 
